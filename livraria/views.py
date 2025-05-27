0715bd6d5db356bb3f403022b8ebe408c517d315
Ezequiel Magalhaes <zekiel.dev@gmail.com>
2025-05-27 18:40:26 -0300

    feat: Adicionando as demais classes

--- livraria/views.py
+++ livraria/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from .forms import SignUpForm, addBookForm
 from .models import Book
 from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic import CreateView, UpdateView, DetailView, ListView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -97,3 +99,87 @@ def book_add(request):
     else:
         messages.error(request, 'Você deve star autenticado para adicionar')
         return redirect('home')
+
+from .models import Book
+from .forms import BookForm
+
+# 1. Classe para Criar
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'add_book.html'
+    success_url = reverse_lazy('book_list')
+    
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            book = form.save(commit=False)
+            book.save()
+            messages.success(self.request, "Livro adicionado com sucesso!")
+            return super().form_valid(form)
+        else:
+            messages.error(self.request, "Você precisa estar logado para adicionar!")
+            return redirect('home')
+
+# 2. Classe para Editar
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'edit_book.html'
+    success_url = reverse_lazy('book_list')
+    
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            book = form.save(commit=False)
+            book.save()
+            messages.success(self.request, "Livro editado com sucesso!")
+            return super().form_valid(form)
+        else:
+            messages.error(self.request, "Você precisa estar logado para editar!")
+            return redirect('home')
+
+# 3. Classe para Detalhar
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    context_object_name = 'book'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Precisa estar logado para visualizar o livro em específico, se não retorna erro
+        if self.request.user.is_authenticated:
+            book = self.get_object()
+            context['book'] = book
+        else:
+            messages.error(self.request, 'Você precisa estar logado!')
+            return redirect('home')
+        return context
+
+# 4. Classe para Listar
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return Book.objects.all()
+        return queryset
+
+# 5. Classe para Deletar
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book_list')
+    
+    def delete(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            try:
+                book = self.get_object()
+                book.delete()
+                messages.success(self.request, 'Livro excluído com sucesso!')
+            except Book.DoesNotExist:
+                messages.error(self.request, 'Livro não encontrado.')
+        else:
+            messages.error(self.request, 'Você precisa estar logado!')
+            return redirect('home')
+        return super().delete(request, *args, **kwargs)
